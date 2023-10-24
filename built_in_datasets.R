697be2eb4dfa7f2bdd3e2a76d2642c07eb9811e1
Ali <razaalirafique@gmail.com>
2023-10-24 18:59:44 -0400

    update

---
+++ built_in_datasets.R
@@ -0,0 +1,20 @@
+# Built-in datasets in R----
+library(help="datasets")
+
+# R datasets
+?data
+
+# Check available datasets in R
+data()
+
+# Load iris dataset in R
+data("iris")
+
+# Find information about iris dataset
+help(iris)
+
+# Print first few rows of the dataset
+head(iris)
+
+# Structure of the dataset
+str(iris)
