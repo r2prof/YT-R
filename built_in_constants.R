eed01977b96fa1b02b2e02f0c67cc1d64f7ebbf3
Ali <razaalirafique@gmail.com>
2023-10-24 19:33:21 -0400

    update

---
+++ built_in_constants.R
@@ -0,0 +1,16 @@
+# How to access built-in constants in R
+
+# Capital Letters in R
+
+LETTERS
+# Small Letters in R
+letters
+
+# Months Information in R
+month.name
+
+# Month Abbreviations in R
+month.abb
+
+# pi value in R
+pi
