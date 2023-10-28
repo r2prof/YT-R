348e9836938621b93664978eba8795c465586256
Ali <razaalirafique@gmail.com>
2023-10-28 18:53:35 -0400

    update

--- mean_var_sd.R
+++ mean_var_sd.R
@@ -1,20 +1,16 @@
+# How to calculate mean, variance and standard dev?
 # Create a sample data set 
-data <- c(12, 15, 18, 21, NA, 27, 30, 33, 36, 39,  
-          45, 48, NA, 54, 57, NA, 63, 66, 69, 42)
-
-summary(data)
+data <- c(12, 15, 18, NA, 21, 27, 30, 33, 36, 39,  
+          45, 48, 54, 57, NA, 63, NA, 66, 69, 42)
 
 
 # Calculate the mean
-mean_result <- mean(data, na.rm = TRUE)
-mean_result
+(mean_data <- mean(data, na.rm = TRUE))
+mean_data
 
 # Calculate the variance
-var_result <- var(data, na.rm = TRUE)
-var_result
-
+(var_data <- var(data, na.rm = TRUE))
 
 # Calculate the standard deviation
-sd_result <- sd(data, na.rm = TRUE)
-sd_result
-
+sd_data <- sd(data, na.rm = TRUE)
+sd_data
