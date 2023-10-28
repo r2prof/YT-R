511b0e2ed605b059ac2ff4ebdf3c323534cbef8d
Ali <razaalirafique@gmail.com>
2023-10-27 22:44:45 -0400

    update

--- mean_var_sd.R
+++ mean_var_sd.R
@@ -1,3 +1,20 @@
-# Create a sample dataset 
-data <- c(12, 15, 18, 21, 24, 27, 30, 33, 36, 39,  
-          45, 48, 51, 54, 57, 60, 63, 66, 69, 42)
+# Create a sample data set 
+data <- c(12, 15, 18, 21, NA, 27, 30, 33, 36, 39,  
+          45, 48, NA, 54, 57, NA, 63, 66, 69, 42)
+
+summary(data)
+
+
+# Calculate the mean
+mean_result <- mean(data, na.rm = TRUE)
+mean_result
+
+# Calculate the variance
+var_result <- var(data, na.rm = TRUE)
+var_result
+
+
+# Calculate the standard deviation
+sd_result <- sd(data, na.rm = TRUE)
+sd_result
+
