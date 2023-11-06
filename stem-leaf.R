226b2b8874837ab497e078dfd6b8e439d7959c7f
Ali <razaalirafique@gmail.com>
2023-11-05 19:37:43 -0500

    update

---
+++ stem-leaf.R
@@ -0,0 +1,18 @@
+# How to create stem-and-leaf plot in R?-----
+
+# Sample data set
+data <- c(34, 48, 62, 51, 37, 45, 58, 42, 49, 53,
+          67, 55, 39, 47, 60, 36, 52, 44, 56, 41,
+          59, 46, 63, 38, 52)
+
+length(data)
+
+sort(data)
+
+# Create a stem-and-leaf plot
+stem(data)
+
+
+
+
+
