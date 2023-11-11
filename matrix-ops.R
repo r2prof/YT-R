efc43ea871a742c69ed94a8bdcaf19fce42e95ff
Ali <razaalirafique@gmail.com>
2023-11-11 18:35:44 -0500

    update

--- matrix-ops.R
+++ matrix-ops.R
@@ -1,16 +1,52 @@
+# How to create  a matrix in R?-----
+
+# In R, a matrix is a collection of elements of 
+# the same data type (numeric, character, or logical) 
+# arranged into a fixed number of rows and columns. 
+x <- 1:5
+y <- 6:10
+z <- 11:15
+
+# create a matrix where x, y and z are columns
+cbind(x, y, z)
+
+# create a matrix where x, y and z are rows
+rbind(x, y, z)
+
+# Create a matrix of the integers 1:10,
+#  with 5 rows and 2 columns
+
+matrix(data = 1:10, nrow = 5, ncol = 2)
+
+# Now with 2 rows and 5 columns
+matrix(data = 1:10, nrow = 2, ncol = 5)
+
+# Now with 2 rows and 5 columns, but fill 
+# by row instead of columns
+matrix(data = 1:10, nrow = 2, ncol = 5, byrow = TRUE)
+
+
+# How to perform matrix operations in R?-----
+
+# how to create a matrix - using matrix function
+AA <- matrix(c(10, 8, 5, 12), ncol = 2, byrow = TRUE)
+
+AA
+
+# how to give names to rows and columns to a matrix
+rownames(AA) <- c("R1", "R2")
+colnames(AA) <- c("C1", "C2")
+
+AA
+
+# how to create a matrix - using matrix function
 A <- matrix(c(10, 8, 5, 12), ncol = 2, byrow = TRUE)
-
-A
-
-rownames(A) <- c("R1", "R2")
-colnames(A) <- c("C1", "C2")
-
-A
 
 B <- matrix(c(5, 3, 15, 6), ncol = 2, byrow = TRUE)
 
 B
 
+# dimensions of a matrix
 dim(A)
 
 # addition of A and B
@@ -32,6 +68,11 @@ A * B
 #   dimensions are compatible. The number of columns 
 #   of the first matrix must be equal to the number 
 #   of rows of the second.
+dim(A)
+
+dim(B)
+
+
 A %*% B
 
 # determinant of a matrix in R
@@ -39,6 +80,8 @@ det(A)
 
 # inverse of a matrix in R
 A_inv <- solve(A)
+
+A_inv
 
 # As a matrix multiplied by its inverse is the 
 # identity matrix we can verify that the previous 
