8dcb39ed8557d65cc3ec2b52a861ae8e1cfaef53
Ali <razaalirafique@gmail.com>
2023-11-19 17:31:46 -0500

    update

--- dotchart.R
+++ dotchart.R
@@ -1,13 +1,8 @@
 # How to make a dot chart in R?------
 
 # Sample data
-categories <- c("A", "B", "C", "D")
+ctypes <- c("A", "B", "C", "D")
 
-values <- c(15, 10, 8, 12)
+numc <- c(15, 10, 8, 12)
 
 # Create a dot chart with custom colors
-dotchart(values, categories, 
-         cex = 1,                       # set the size of the dots
-         pch = 19,                      # set the plotting char to circle
-         main = "Dot Chart",
-         color = c("blue", "green", "orange", "red"))
