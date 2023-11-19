71171b472b85bd337761ac5274af0340b65c06a5
Ali <razaalirafique@gmail.com>
2023-11-19 17:47:02 -0500

    update

---
+++ seq_nums.R
@@ -0,0 +1,32 @@
+# Hwo to create of sequence of numbers in R?----
+
+# Create a sequence from 1 to 10
+seq_one_ten <- 1:10
+
+print(seq_one_ten)
+
+# Create a sequence of even numbers from 2 to 20
+even_numbers <- seq(from = 2, to = 20, by = 2)
+
+print(even_numbers)
+
+# Create a sequence of odd numbers from 1 to 15
+
+odd_numbers <- seq(from = 1, to = 15, by = 2)
+
+print(odd_numbers)
+
+# Create a sequence from 5 to 50 with a step of 5
+seq_step_5 <- seq(from = 5, to = 50, by = 5)
+
+print(seq_step_5)
+
+# Create a sequence from 0 to 1 with 0.1 increments
+seq_zero_to_one <- seq(from = 0, to = 1, by = 0.1)
+
+print(seq_zero_to_one)
+
+# Create a decreasing sequence from 10 to 1
+dec_seq <- seq(from = 10, to = 1, by = -1)
+
+print(dec_seq)
