d8d99c774c2ea28e97ad8851af589391f4cb5e58
Ali <razaalirafique@gmail.com>
2023-11-19 18:39:16 -0500

    update

--- output_save_csv.R
+++ output_save_csv.R
@@ -11,5 +11,13 @@ data <- data.frame(
 data$Age <- data$Age + 5
 
 # Save the updated data to a CSV file
-write.csv(data, "output_data.csv", row.names = FALSE)
+write.csv(data, "score.csv", row.names = FALSE)
 
+
+# Create a vector
+rain_mm <- 1:25
+
+print(rain_mm)
+
+# Save the data to a CSV file
+write.csv(data, "rain.csv", row.names = FALSE)
