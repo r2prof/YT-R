d21d09efa1d47cb7881326fd92d3ec218bb5efca
Ali <razaalirafique@gmail.com>
2023-11-26 21:42:16 -0500

    update

--- control_decimal.R
+++ control_decimal.R
@@ -1,12 +1,13 @@
 # How to control decimal places in R?------
 
 # Basic Syntax of round()
-rounded_value <- round(original_value, digits = decimal_places)
+rounded_value <- round(original_value, 
+                       digits = decimal_places)
 
 # Example
 original_value <- 3.141592653589793
 
-rounded_value <- round(original_value, digits = 2)
-
+rounded_value <- round(original_value,
+                       digits = 3)
 # Output
 rounded_value
